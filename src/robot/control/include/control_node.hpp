1e9994534224d01dd4bc910b7c7b2861cb5ce6f7
Owen Leather <owenleather03@gmail.com>
2023-09-23 05:06:40 +0000

    added dependencies to control node and imports

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -3,6 +3,15 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "control_core.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "tf2/exceptions.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "std_msgs/msg/string.h"
 
 class ControlNode : public rclcpp::Node {
   public:
