1231852b208ab39760b9c0db1297d00ac913e44c
Simon Wills <s2wills@uwaterloo.ca>
2023-10-07 01:14:04 +0000

    training

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -3,10 +3,48 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "control_core.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "tf2/exceptions.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 class ControlNode : public rclcpp::Node {
   public:
     ControlNode();
+
+    // Deliverable 5.1
+    rclcpp::TimerBase::SharedPtr timer_;
+    void timer_callback();
+
+    // Deliverable 5.2
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
+    // Deliverable 5.3
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscriber_;
+    void subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+    // Deliverable 6.1
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goalsub_;
+    void goalsub_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+
+    // Deliverable 6.2
+    rclcpp::TimerBase::SharedPtr movetimer_;
+    void movetimer_callback();
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    geometry_msgs::msg::PointStamped goal_point;
+
+    // Deliverable 6.3
+    double Kp_linear = 0.5;
+    double Kp_angular = 0.5;
+
+    // Deliverable 6.4
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twistpublisher_;
+
 
   private:
     robot::ControlCore control_;
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -2,9 +2,63 @@
 
 #include "control_node.hpp"
 
+void ControlNode::timer_callback(){
+  // Deliverable 5.1
+  RCLCPP_INFO(get_logger(), "Timer for 5.1");
+
+  // Deliverable 5.2
+  auto msg = std_msgs::msg::String();
+  msg.data = "Publish for 5.2";
+  publisher_->publish(msg);
+}
+
+// Deliverable 5.3
+void ControlNode::subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  auto x = msg->pose.pose.position.x;
+  auto y = msg->pose.pose.position.y;
+  auto z = msg->pose.pose.position.z;
+  RCLCPP_INFO(get_logger(), "X: %f | Y: %f | Z: %f", x, y, z);
+}
+
+// Deliverable 6.1
+void ControlNode::goalsub_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg) {
+  goal_point = *msg;
+  auto x = msg->point.x;
+  auto y = msg->point.y;
+  auto z = msg->point.z;
+  RCLCPP_INFO(get_logger(), "Goal Point: \nX: %f | Y: %f | Z: %f", x, y, z);
+}
+
+void ControlNode::movetimer_callback() {
+  //RCLCPP_INFO(get_logger(), "Timer Test");
+
+  geometry_msgs::msg::TransformStamped transform;
+  try {
+    transform = tf_buffer_->lookupTransform("robot", "sim_world", tf2::TimePointZero);
+  } catch (const tf2::TransformException & ex) {
+    RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
+  }
+  auto transformed_point = geometry_msgs::msg::PointStamped();
+  tf2::doTransform(goal_point, transformed_point, transform);
+
+  geometry_msgs::msg::Twist msg = geometry_msgs::msg::Twist();
+  msg.linear.x = transformed_point.point.x * Kp_linear;
+  msg.angular.z = transformed_point.point.y * Kp_angular;
+  twistpublisher_->publish(msg);
+}
+
 ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 {
+  //timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&ControlNode::timer_callback, this));
+  publisher_ = this->create_publisher<std_msgs::msg::String>("/example_string", 20);
+  subscriber_= this->create_subscription<nav_msgs::msg::Odometry>("/model/robot/odometry", 20, std::bind(&ControlNode::subscription_callback, this, std::placeholders::_1));
+  goalsub_ = this->create_subscription<geometry_msgs::msg::PointStamped>("/goal_point", 20, std::bind(&ControlNode::goalsub_callback, this, std::placeholders::_1));
   
+  movetimer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&ControlNode::movetimer_callback, this));
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  twistpublisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 }
 
 int main(int argc, char ** argv)
