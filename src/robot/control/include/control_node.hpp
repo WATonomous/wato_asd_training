fe50d797c13729f1712fc4364cd1d093cb3c9a06
Albert <albertnt905@gmail.com>
2023-11-05 04:29:09 +0000

    finished training module

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -10,15 +10,47 @@
 #include "tf2/exceptions.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "std_msgs/msg/string.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
 
 class ControlNode : public rclcpp::Node {
   public:
     ControlNode();
+    double Kp_linear = 0.5;
+    double Kp_angular = 0.5;
 
   private:
     robot::ControlCore control_;
+    void timer_callback();
+    void subscription_callback(
+      const nav_msgs::msg::Odometry::SharedPtr msg
+    );
+    void goalSubscription_callback(
+      const geometry_msgs::msg::PointStamped::SharedPtr msg
+    );
+    void goalTimer_callback();
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::TimerBase::SharedPtr goalTimer_;
+  
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr control_publisher_;
+
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscriber_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goalSubscriber_;
+
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+    geometry_msgs::msg::PointStamped goal_point;
+
+    double goal_point_transformed_x;
+    double goal_point_transformed_y;
+
 };
+
+
 
 #endif
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -5,6 +5,84 @@
 ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 {
   
+  /*timer_ = this->create_wall_timer(
+    std::chrono::milliseconds(1000),
+    std::bind(&ControlNode::timer_callback, this));
+
+  publisher_ =
+    this->create_publisher<std_msgs::msg::String>("/example_string", 20);
+  
+  subscriber_ =
+    this->create_subscription<nav_msgs::msg::Odometry>("/model/robot/odometry", 20,
+    std::bind(&ControlNode::subscription_callback, this, std::placeholders::_1));
+  */
+
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+  goalSubscriber_ = 
+    this->create_subscription<geometry_msgs::msg::PointStamped>("/goal_point", 20,
+    std::bind(&ControlNode::goalSubscription_callback, this, std::placeholders::_1));
+
+  goalTimer_ = this->create_wall_timer(
+    std::chrono::milliseconds(100),
+    std::bind(&ControlNode::goalTimer_callback, this));
+
+  control_publisher_ = 
+    this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 20);
+}
+
+void ControlNode::timer_callback()
+{
+  RCLCPP_INFO(this->get_logger(), "Hello World");
+  auto msg = std_msgs::msg::String();
+  msg.data = "First topic!";
+  publisher_->publish(msg);
+}
+
+void ControlNode::subscription_callback(
+  const nav_msgs::msg::Odometry::SharedPtr msg) 
+{
+  auto x = msg->pose.pose.position.x;
+  auto y = msg->pose.pose.position.y;
+  auto z = msg->pose.pose.position.z;
+
+  RCLCPP_INFO(this->get_logger(), "Received position: x=%f, y=%f, z=%f", x, y, z);
+}
+
+void ControlNode::goalSubscription_callback(
+  const geometry_msgs::msg::PointStamped::SharedPtr msg)
+{
+  goal_point = *msg;
+  RCLCPP_INFO(this->get_logger(), "Goal point: x=%f, y=%f, z=%f", goal_point.point.x, goal_point.point.y, goal_point.point.z);
+
+}
+
+void ControlNode::goalTimer_callback()
+{
+  geometry_msgs::msg::TransformStamped transform;
+
+  try {
+    transform = tf_buffer_->lookupTransform("robot", "sim_world", tf2::TimePointZero);
+  } catch (const tf2::TransformException & ex) {
+    RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
+  }
+
+  auto transformed_point = geometry_msgs::msg::PointStamped();
+
+  tf2::doTransform(goal_point, transformed_point, transform);
+
+  RCLCPP_INFO(this->get_logger(), "Goal point: x=%f, y=%f, z=%f", transformed_point.point.x, transformed_point.point.y, transformed_point.point.z);
+
+  auto control_point = geometry_msgs::msg::Twist();
+
+  control_point.linear.x = Kp_linear * transformed_point.point.x;
+  control_point.angular.z = Kp_angular * transformed_point.point.y;
+
+  control_publisher_->publish(control_point);
+
+  RCLCPP_INFO(this->get_logger(), "Successfully published control point");
+
 }
 
 int main(int argc, char ** argv)
