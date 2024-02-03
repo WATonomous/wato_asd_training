9134bc08e40dec3275290f7ba214a0e3286b652a
Roozbeh Ali <roozbeh.ali.2005@gmail.com>
2024-02-03 00:07:16 +0000

    Deliverable 5.3

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -11,7 +11,8 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
-#include "std_msgs/msg/string.h"
+#include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 class ControlNode : public rclcpp::Node {
   public:
@@ -22,9 +23,12 @@ class ControlNode : public rclcpp::Node {
 
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr msg_pub_;
 
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
+
     rclcpp::TimerBase::SharedPtr timer_;
 
     void timer_callback();
+    void odom_callback(nav_msgs::msg::Odometry::SharedPtr msg);
 };
 
 #endif
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -6,7 +6,17 @@
 ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 {
   msg_pub_ = 
-    this->create_publisher<std_msgs::msg::String>("/example_string", 20);
+    this->create_publisher<std_msgs::msg::String>(
+      "/example_string",
+      20
+    );
+  
+  odom_subscriber_ = 
+    this->create_subscription<nav_msgs::msg::Odometry>(
+      "/model/robot/odometry",
+      20,
+      std::bind(&ControlNode::odom_callback, this, std::placeholders::_1)
+    );
 
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(1000), 
@@ -14,11 +24,19 @@ ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 }
 
 void ControlNode::timer_callback() {
-  RCLCPP_INFO(this->get_logger(), "Hello World...");
+  // RCLCPP_INFO(this->get_logger(), "Hello World...");
 
   auto msg = std_msgs::msg::String();
   msg.data = "Hello example_string topic...";
   msg_pub_->publish(msg);
+}
+
+void ControlNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  auto xpos = msg->pose.pose.position.x;
+  auto ypos = msg->pose.pose.position.y;
+  auto zpos = msg->pose.pose.position.z;
+
+  RCLCPP_INFO(this->get_logger(), "Position co-ords: x=%f, y=%f, z=%f", xpos, ypos, zpos);
 }
 
 int main(int argc, char ** argv)
