dc450616f5a95e53edb7f61cdae393a954aef1d4
Andrew <andrew.shum068@gmail.com>
2024-02-12 18:32:55 +0000

    edited watod-config.sh

--- watod-config.sh
+++ watod-config.sh
@@ -8,15 +8,14 @@
 ##   - robot            :   starts up robot nodes
 ##   - samples          :   starts up sample nodes for reference
 
-# ACTIVE_PROFILES=""
-
+ACTIVE_PROFILES="samples"
 
 ## Name to append to docker containers. DEFAULT = <your_watcloud_username>
 
-# COMPOSE_PROJECT_NAME=""
+ COMPOSE_PROJECT_NAME="ashum68"
 
 
 ## Tag to use. Images are formatted as <IMAGE_NAME>:<TAG> with forward slashes replaced with dashes.
 ## DEFAULT = <your_current_github_branch> 
 
-# TAG=""
+ TAG="andrew_shum_training"
