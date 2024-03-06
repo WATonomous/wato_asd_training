d868412168cbd7bf2f9d36bf40f6248ecb96e659
Andrew <andrew.shum068@gmail.com>
2024-03-06 08:43:13 +0000

    Completed ASD Assignment

--- watod-config.sh
+++ watod-config.sh
@@ -8,14 +8,14 @@
 ##   - robot            :   starts up robot nodes
 ##   - samples          :   starts up sample nodes for reference
 
-ACTIVE_PROFILES="samples"
+ACTIVE_PROFILES="vis_tools gazebo robot"
 
 ## Name to append to docker containers. DEFAULT = <your_watcloud_username>
 
- COMPOSE_PROJECT_NAME="ashum68"
+ #COMPOSE_PROJECT_NAME="ashum68"
 
 
 ## Tag to use. Images are formatted as <IMAGE_NAME>:<TAG> with forward slashes replaced with dashes.
 ## DEFAULT = <your_current_github_branch> 
 
- TAG="andrew_shum_training"
+ #TAG="andrew_shum_training"
