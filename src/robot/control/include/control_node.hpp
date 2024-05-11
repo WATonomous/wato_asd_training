615f03abc051ca53dc481aacc6ae489fbe11e152
akilpath <akilpath@gmail.com>
2024-05-11 03:28:51 +0000

    Deliverables 6 (not going to location)

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -12,7 +12,6 @@
 #include "tf2_ros/buffer.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "std_msgs/msg/string.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 
 class ControlNode : public rclcpp::Node {
   public:
@@ -21,10 +20,15 @@ class ControlNode : public rclcpp::Node {
   private:
     robot::ControlCore control_;
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscriber_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    geometry_msgs::msg::PointStamped last_goal_point;
+
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+      
     void timer_callback();
-    void subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void subscription_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
 
 
 };
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -2,30 +2,57 @@
 
 #include "control_node.hpp"
 
-#define timer_delay 1500
+#define timer_delay 100
+#define Kp_linear 0.5
+#define Kp_angular 0.5
+
 ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 {
   timer_ = this->create_wall_timer(std::chrono::milliseconds(timer_delay), std::bind(&ControlNode::timer_callback, this));
-  publisher_ = this->create_publisher<std_msgs::msg::String>("/example_string", 20);
 
-  subscriber_ = this -> create_subscription<nav_msgs::msg::Odometry>(
-                        "/model/robot/odometry", 20, 
+  publisher_ = this -> create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 20);
+  subscriber_ = this -> create_subscription<geometry_msgs::msg::PointStamped>(
+                        "/goal_point", 20, 
                         std::bind(&ControlNode::subscription_callback, this, std::placeholders::_1));
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_); 
+                        
+
 }
 
 void ControlNode::timer_callback(){
-  auto message = std_msgs::msg::String();
-  message.data = "Published message";
-  publisher_ -> publish(message);
-  RCLCPP_INFO(this->get_logger(), "Deliverables");
+  float control_linear = Kp_linear * last_goal_point.point.x;
+  float control_angular = Kp_angular * last_goal_point.point.y;
+
+  auto twist_cmd = geometry_msgs::msg::Twist();
+  twist_cmd.linear.x = control_linear;
+  twist_cmd.angular.z = control_angular;
+  publisher_ -> publish(twist_cmd);
+  //RCLCPP_INFO(this->get_logger(), "Timer callback");
+  // RCLCPP_INFO(this->get_logger(), "LAST GPOINT COORDS (x,y,z): (%f, %f, %f)", 
+  //                 last_goal_point.point.x,
+  //                 last_goal_point.point.y,
+  //                 last_goal_point.point.z);
 }
 
-void ControlNode::subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+void ControlNode::subscription_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg)
 {
-  float x = msg -> pose.pose.position.x;
-  float y = msg -> pose.pose.position.y;
-  float z = msg -> pose.pose.position.z;
-  RCLCPP_INFO(this->get_logger(), "Coordinates (x,y,z): (%f, %f, %f)", x,y,z);
+  
+  geometry_msgs::msg::TransformStamped transform;
+  auto transformed_point = geometry_msgs::msg::PointStamped();
+  try {
+      transform = tf_buffer_->lookupTransform("sim_world", "robot", tf2::TimePointZero);
+  } catch (const tf2::TransformException & ex) {
+      RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
+  }
+
+  tf2::doTransform(*msg, transformed_point, transform);
+  float x = transformed_point.point.x;
+  float y = transformed_point.point.y;
+  float z = transformed_point.point.z;
+  last_goal_point = transformed_point;
+
+  RCLCPP_INFO(this->get_logger(), "GOAL POINT COORDS (x,y,z): (%f, %f, %f)", x,y,z);
 }
 
 int main(int argc, char ** argv)
