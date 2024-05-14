12132f98bb9221fb6f44b92b1cb168c50d4d066a
akilpath <akilpath@gmail.com>
2024-05-14 01:53:08 +0000

    Works now

--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -21,38 +21,36 @@ ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 }
 
 void ControlNode::timer_callback(){
-  float control_linear = Kp_linear * last_goal_point.point.x;
-  float control_angular = Kp_angular * last_goal_point.point.y;
+
+  geometry_msgs::msg::TransformStamped transform;
+  auto transformed_point = geometry_msgs::msg::PointStamped();
+  try {
+      transform = tf_buffer_->lookupTransform("robot", "sim_world", tf2::TimePointZero);
+  } catch (const tf2::TransformException & ex) {
+      RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
+  }
+
+  tf2::doTransform(last_goal_point, transformed_point, transform);
+  RCLCPP_INFO(this->get_logger(), "TRANSFORMED_POINT COORDS (x,y,z): (%f, %f, %f)", 
+                  transformed_point.point.x,
+                  transformed_point.point.y,
+                  transformed_point.point.z);
+  
+
+  float control_linear = Kp_linear * transformed_point.point.x;
+  float control_angular = Kp_angular * transformed_point.point.y;
 
   auto twist_cmd = geometry_msgs::msg::Twist();
   twist_cmd.linear.x = control_linear;
   twist_cmd.angular.z = control_angular;
   publisher_ -> publish(twist_cmd);
   //RCLCPP_INFO(this->get_logger(), "Timer callback");
-  // RCLCPP_INFO(this->get_logger(), "LAST GPOINT COORDS (x,y,z): (%f, %f, %f)", 
-  //                 last_goal_point.point.x,
-  //                 last_goal_point.point.y,
-  //                 last_goal_point.point.z);
 }
 
 void ControlNode::subscription_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg)
 {
-  
-  geometry_msgs::msg::TransformStamped transform;
-  auto transformed_point = geometry_msgs::msg::PointStamped();
-  try {
-      transform = tf_buffer_->lookupTransform("sim_world", "robot", tf2::TimePointZero);
-  } catch (const tf2::TransformException & ex) {
-      RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
-  }
-
-  tf2::doTransform(*msg, transformed_point, transform);
-  float x = transformed_point.point.x;
-  float y = transformed_point.point.y;
-  float z = transformed_point.point.z;
-  last_goal_point = transformed_point;
-
-  RCLCPP_INFO(this->get_logger(), "GOAL POINT COORDS (x,y,z): (%f, %f, %f)", x,y,z);
+  last_goal_point = *msg;
+  // RCLCPP_INFO(this->get_logger(), "GOAL POINT COORDS (x,y,z): (%f, %f, %f)", x,y,z);
 }
 
 int main(int argc, char ** argv)
