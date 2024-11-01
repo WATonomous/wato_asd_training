42727683fa21fc7003698cf3c5911e139893bc1f
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2024-11-01 04:44:15 +0000

    deliverable 6.4

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -20,12 +20,18 @@ class ControlNode : public rclcpp::Node {
 
   private:
     robot::ControlCore control_;
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscriber_;
+    rclcpp::TimerBase::SharedPtr pid_loop_timer_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goal_point_subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    geometry_msgs::msg::PointStamped goal_point_; // in robot ref
 
-    void timer_callback();
-    void subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg_odom);
+    void pid_loop_callback();
+    void goal_point_subscription_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+
+    const double Kp_linear{0.5};
+    const double Kp_angular{0.5};
 };
 
 #endif
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -4,24 +4,46 @@
 
 ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore())
 {
-  subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>("/model/robot/odometry", 20, std::bind(&ControlNode::subscription_callback, this, std::placeholders::_1));
-  publisher_ = this->create_publisher<std_msgs::msg::String>("/example_string", 20);
-  timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&ControlNode::timer_callback, this));
+  goal_point_subscriber_ = this->create_subscription<geometry_msgs::msg::PointStamped>("/goal_point", 20, std::bind(&ControlNode::goal_point_subscription_callback, this, std::placeholders::_1));
+  cmd_vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 20);
+  pid_loop_timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&ControlNode::pid_loop_callback, this));
+
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 }
 
-void ControlNode::timer_callback() {
-  char sample_str[100];
-  sprintf(sample_str, "Hello WATO! %lu", std::chrono::high_resolution_clock::now().time_since_epoch() / std::chrono::milliseconds(1));
-
-  auto msg = std_msgs::msg::String();
-  msg.data = sample_str;
-
-  publisher_->publish(msg);
-  RCLCPP_INFO(this->get_logger(), "Hello WATO! %lu", std::chrono::high_resolution_clock::now().time_since_epoch() / std::chrono::milliseconds(1));
+void ControlNode::goal_point_subscription_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg) {
+  RCLCPP_INFO(this->get_logger(), "goal point (x,y,z): %f, %f, %f", msg->point.x, msg->point.y, msg->point.z);
+  this->goal_point_ = *msg;
 }
 
-void ControlNode::subscription_callback(const nav_msgs::msg::Odometry::SharedPtr msg_odom) {
-  RCLCPP_INFO(this->get_logger(), "robot odom (x,y,z): %f,%f,%f", msg_odom->pose.pose.position.x, msg_odom->pose.pose.position.y, msg_odom->pose.pose.position.z);
+void ControlNode::pid_loop_callback() {
+  // Transform goal point into robot-space before storing
+  geometry_msgs::msg::TransformStamped transform;
+  try {
+    transform = tf_buffer_->lookupTransform("robot", "sim_world", tf2::TimePointZero);
+  } catch (const tf2::TransformException &ex) {
+    RCLCPP_INFO(this->get_logger(), "Could not transform %s", ex.what());
+  }
+
+  auto transformed_point = geometry_msgs::msg::PointStamped();
+  tf2::doTransform(this->goal_point_, transformed_point, transform);
+
+  double lin_ctrl_signal{Kp_linear * transformed_point.point.x};
+  double ang_ctrl_signal{Kp_angular * transformed_point.point.y};
+
+  auto msg = geometry_msgs::msg::Twist();
+  auto linear_component = geometry_msgs::msg::Vector3();
+  auto angular_component = geometry_msgs::msg::Vector3();
+  linear_component.set__x(lin_ctrl_signal);
+  angular_component.set__z(ang_ctrl_signal);
+
+  msg.set__linear(linear_component);
+  msg.set__angular(angular_component);
+
+  cmd_vel_publisher->publish(msg);
+
+  RCLCPP_INFO(this->get_logger(), "linear/radial mtr outputs: %f, %f", lin_ctrl_signal, ang_ctrl_signal);
 }
 
 int main(int argc, char ** argv)
