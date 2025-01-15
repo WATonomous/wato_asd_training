a6be4bba422ec2e7d1b7ff7095610ba1814ded47
bluebarryz <bzbear16@hotmail.com>
2025-01-14 23:26:04 -0500

    warm up

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,10 +1,22 @@
 #include <chrono>
 #include <memory>
-
+ 
 #include "costmap_node.hpp"
-
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {}
-
+ 
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+  // Initialize the constructs and their parameters
+  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+}
+ 
+// Define the timer to publish a message every 500ms
+void CostmapNode::publishMessage() {
+  auto message = std_msgs::msg::String();
+  message.data = "Hello, ROS 2!";
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  string_pub_->publish(message);
+}
+ 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
