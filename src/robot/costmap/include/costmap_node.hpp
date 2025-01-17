02724a5cf2b7e840779bf6ef9d7a9cfe4a3dd0b1
LSpinoti <spinotil@gmail.com>
2025-01-16 21:30:56 -0500

    Created Costmap

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -3,21 +3,44 @@
  
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
- 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/pose_with_covariance.hpp"
 #include "costmap_core.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
  
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
     
     // Place callback function here
-    void publishMessage();
+    void publishCostmap(const sensor_msgs::msg::LaserScan::SharedPtr laserscan);
+    void inflate(std::vector<int8_t>& grid, int res, double radius);
+    void set(std::vector<int8_t>& grid, int res, int y, int x, int8_t val);
+    int8_t get(std::vector<int8_t> grid, int res, int y, int x);
+    void setPose(const nav_msgs::msg::Odometry::SharedPtr odom);
+    void print(std::string s);
+    
  
   private:
     robot::CostmapCore costmap_;
     // Place these constructs here
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+    double robot_angle;
+    double robot_angle_x;
+    double robot_angle_y;
+    double robot_angle_z;
+    double robot_angle_w;
+    double robot_x;
+    double robot_y;
+    double robot_z;
+
+    rclcpp::Time past_;
 };
  
 #endif 
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -2,21 +2,132 @@
 #include <memory>
  
 #include "costmap_node.hpp"
- 
+
+using std::placeholders::_1;
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
-  // Initialize the constructs and their parameters
   string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::publishCostmap, this, _1));
+  odometry_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom/filtered", 10, std::bind(&CostmapNode::setPose, this, _1));
+  costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
+
+  past_ = this->get_clock()->now();
 }
- 
-// Define the timer to publish a message every 500ms
-void CostmapNode::publishMessage() {
+
+void CostmapNode::publishCostmap(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
+  /*
+    This formula below ensures that at the maximum distance,
+    the maximum gap between laser scans is exactly one cell
+  */
+  int res = 2 / scan->angle_increment;
+
+  /* Scan */
+  std::vector<int8_t> grid(res * res, 0);
+  for (int i = 0; i < scan->ranges.size(); i++) {
+    double angle = scan->angle_min + i * scan->angle_increment + robot_angle;
+    double range = scan->ranges[i];
+    if (range > scan->range_min && range < scan->range_max) {
+      int grid_x = res / 2 + (range / scan->range_max) * -sin(angle) * (res / 2);
+      int grid_y = res / 2 + (range / scan->range_max) * -cos(angle) * (res / 2);
+
+      grid[res * grid_y + grid_x] = 100;
+    }
+  }
+
+  inflate(grid, res, 2.1);
+
+  /* Set up OccupancyGrid */
+  auto occgrid = nav_msgs::msg::OccupancyGrid();
+  occgrid.data = grid;
+  occgrid.info.map_load_time = this->get_clock()->now();
+  occgrid.info.resolution = 2 * scan->range_max / res;
+  occgrid.info.width = res;
+  occgrid.info.height = res;
+  occgrid.info.origin.position.x = robot_x - scan->range_max;
+  occgrid.info.origin.position.y = robot_y - scan->range_max;
+  occgrid.info.origin.position.z = robot_z;
+  occgrid.info.origin.orientation.x = 0;
+  occgrid.info.origin.orientation.y = 0;
+  occgrid.info.origin.orientation.z = 0;
+  occgrid.info.origin.orientation.w = 1.0;
+  
+  costmap_pub_->publish(occgrid);
+  
+  /* Print grid */
+  // auto clock = this->get_clock()->now();
+  // if (clock > past_ + rclcpp::Duration::from_seconds(2))
+  // {
+  //   past = clock;
+  //   std::string out = "";
+  //   for (int i = 0; i < res; i++) {
+  //     for (int j = 0; j < res; j++) {
+  //       if (i == res / 2 && j == res / 2)
+  //         out += "@@";
+  //       else if (grid[res * i + j] == 100)
+  //         out += "██";
+  //       else if (grid[res * i + j] > 0)
+  //         out += "##";
+  //       else
+  //         out += "  ";
+  //     }
+  //     out += "\n";
+  //   }
+  //   print(out);
+  // }
+}
+
+void CostmapNode::inflate(std::vector<int8_t>& grid, int res, double radius) {
+  int intrad = ceil(radius);
+  for (int y = 0; y < res; y++) {
+    for (int x = 0; x < res; x++) {
+      if (grid[y * res + x] == 100) {
+        for (int i = -intrad; i <= intrad; i++) {
+          for (int j = -intrad; j <= intrad; j++) {
+            double dist = sqrt(i * i + j * j);
+
+            if (dist < radius) {
+              int curcost = get(grid, res, y + i, x + j);
+              int newcost = 100.0 * (1.0 - dist / radius);
+
+              if (newcost > curcost) {
+                set(grid, res, y + i, x + j, newcost);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+void CostmapNode::set(std::vector<int8_t>& grid, int res, int y, int x, int8_t val) {
+  if (y >= 0 && y < res && x >= 0 && x < res) {
+    grid[res * y + x] = val;
+  }
+}
+int8_t CostmapNode::get(std::vector<int8_t> grid, int res, int y, int x) {
+  if (y >= 0 && y < res && x >= 0 && x < res)
+    return grid[res * y + x];
+  return 100;
+}
+
+void CostmapNode::setPose(const nav_msgs::msg::Odometry::SharedPtr odom) {
+  tf2::Quaternion quat_tf;
+  tf2::fromMsg(odom->pose.pose.orientation, quat_tf);
+  double r{}, p{}, y{};
+  tf2::Matrix3x3 m(quat_tf);
+  m.getRPY(r, p, y);
+  robot_angle = y;
+  robot_x = odom->pose.pose.position.x;
+  robot_y = odom->pose.pose.position.y;
+  robot_z = odom->pose.pose.position.z;
+}
+
+void CostmapNode::print(std::string s) {
   auto message = std_msgs::msg::String();
-  message.data = "Hello, ROS 2!";
+  message.data = s;
   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
   string_pub_->publish(message);
 }
- 
+
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
