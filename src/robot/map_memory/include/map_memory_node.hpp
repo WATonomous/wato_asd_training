c79d8128abd859eb5b867006428fe0e24a51994b
bluebarryz <bzbear16@hotmail.com>
2025-01-18 20:06:23 -0500

    quaternion to yaw conversion

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -25,9 +25,11 @@ class MapMemoryNode : public rclcpp::Node {
 	void updateMap();
 
 	// Flags
-	double last_x = -100;
-	double last_y = -100;
-    const double distance_threshold;
+	double robot_x_ = -100;
+	double robot_y_ = -100;
+	double robot_yaw_;
+	double quaternionToYaw(const geometry_msgs::msg::Quaternion& q);
+    const double distance_threshold_;
     bool costmap_updated_ = false;
     nav_msgs::msg::OccupancyGrid::SharedPtr latest_costmap_;
     bool should_update_map_ = false;
--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,6 +1,6 @@
 #include "map_memory_node.hpp"
 
-MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())), distance_threshold(5) {
+MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())), distance_threshold_(5) {
 	costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/costmap", 10, 
 		std::bind(
 			&MapMemoryNode::costmap_callback, this,
@@ -23,17 +23,24 @@ void MapMemoryNode::odom_callback(nav_msgs::msg::Odometry::SharedPtr odom_msg) {
 	double y = odom_msg->pose.pose.position.y;
 
 	// Compute distance traveled
-	double distance = std::sqrt(std::pow(x - last_x, 2) + std::pow(y - last_y, 2));
-	if (distance >= distance_threshold) {
-		last_x = x;
-		last_y = y;
+	double distance = std::sqrt(std::pow(x - robot_x_, 2) + std::pow(y - robot_y_, 2));
+	if (distance >= distance_threshold_) {
+		robot_x_ = x;
+		robot_y_ = y;
+		robot_yaw_ = quaternionToYaw(odom_msg->pose.pose.orientation);
 		should_update_map_ = true;
 	}
 }
 
+double MapMemoryNode::quaternionToYaw(const geometry_msgs::msg::Quaternion& q) {
+	double siny_cosp = 2.0 * (q.w * q.z + q.x * q.y);
+	double cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+	return std::atan2(siny_cosp, cosy_cosp);
+}
+
 void MapMemoryNode::updateMap() {
 	if (should_update_map_ && costmap_updated_) {
-		nav_msgs::msg::OccupancyGrid::SharedPtr global_map = map_memory_.integrateCostmap(latest_costmap_);
+		nav_msgs::msg::OccupancyGrid::SharedPtr global_map = map_memory_.integrateCostmap(latest_costmap_, robot_x_, robot_y_, robot_yaw_);
 		map_pub_->publish(*global_map);
 		should_update_map_ = false;
 	}
